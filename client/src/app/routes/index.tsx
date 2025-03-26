4dbd7724a5af9b4bda757b1cdf02a5b3b60f4b1e
Durukar <nightdavila@gmail.com>
2025-03-25 22:22:30 -0300

    feat: Configuração inicial da rota

--- client/src/app/routes/index.tsx
+++ client/src/app/routes/index.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router"
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+import { AuthLayout } from '../layout/auth'
+import { AuthPage } from '../pages/auth'
 
 const RouterApp = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-        
+          <Route element={<AuthLayout />}>
+            <Route index element={<AuthPage />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
   )
 }
 
-export {RouterApp}
+export { RouterApp }
